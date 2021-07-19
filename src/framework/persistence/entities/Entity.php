80fa2bb96b343660f4569e86cdfab6d68d52556f
Antoine Tophin <86663639+AntoineTophin@users.noreply.github.com>
2021-07-19 11:51:30 +0200

    feature/entities: Entity comments

--- src/framework/persistence/entities/Entity.php
+++ src/framework/persistence/entities/Entity.php
@@ -8,25 +8,56 @@ use PHPeacock\Framework\Persistence\Connections\DBMSConnection;
  */
 abstract class Entity implements InsertEntity, UpdateEntity, DeleteEntity
 {
+
+    /**
+     * Related database management system connection.
+     * @var DBMSConnection|null $dbmsConnection
+     */
     protected ?DBMSConnection $dbmsConnection;
 
+    /**
+     * Unique id for database storage.
+     * @var int|null $id
+     */
     protected ?int $id;
 
+    /**
+     * Returns the id.
+     * 
+     * @return int
+     */
     public function getId(): int
     {
         return $this->id;
     }
 
+    /**
+     * Sets the id property.
+     * 
+     * @param int $id Entity id.
+     * @return void
+     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
+    /**
+     * Returns the DBMS connection.
+     * 
+     * @return DBMSConnection
+     */
     public function getDBMSConnection(): DBMSConnection
     {
         return $this->dbmsConnection;
     }
 
+    /**
+     * Sets the dbmsConnection property.
+     * 
+     * @param DBMSConnection $dbmsConnection Related DBMS connection.
+     * @return void
+     */
     public function setDBMSConnection(DBMSConnection $dbmsConnection): void
     {
         $this->dbmsConnection = $dbmsConnection;
