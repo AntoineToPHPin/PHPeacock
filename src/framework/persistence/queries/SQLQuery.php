49a6a4a5115cc7a72ecfb538d2a06d91ffd198b3
Antoine Tophin <antoine-externe.tophin@enedis.fr>
2021-08-19 08:35:47 +0200

    features/persistence: queries refactoring

--- src/framework/persistence/queries/SQLQuery.php
+++ src/framework/persistence/queries/SQLQuery.php
@@ -3,9 +3,15 @@ namespace PHPeacock\Framework\Persistence\Queries;
 
 /**
  * Makes every SQL query builder stringable.
- * 
- * The query builders __toString method must return
- * a correct SQL query.
  */
 interface SQLQuery extends \Stringable
-{ }
+{
+    /**
+     * Returns a correct SQL query.
+     * 
+     * @throws QueryException if an error occurs when building a query.
+     * 
+     * @return string
+     */
+    public function __toString(): string;
+}
