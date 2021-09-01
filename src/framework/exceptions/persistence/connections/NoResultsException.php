73f7ad902c946717e057c9b2140890f96eaf26d2
Antoine Tophin <86663639+AntoineTophin@users.noreply.github.com>
2021-09-01 07:42:00 +0200

    features/autoloader: remove file

--- src/framework/exceptions/persistence/connections/NoResultsException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace PHPeacock\Framework\Exceptions\Persistence\Connections;
-
-/**
- * Exception for no results returned.
- */
-class NoResultsException extends ConnectionException
-{ }
